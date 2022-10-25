f3a0d74891cfca9e6050dd99aedbf76e1dc9c7b3
giacatho <giacatho@gmail.com>
2022-10-25 16:40:20 +0800

    [validator workshop] start the workshop project

---
+++ spring-validation-workshop/src/main/java/sg/nus/iss/validator/workshop/SpringValidationWorkshopApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.validator.workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringValidationWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringValidationWorkshopApplication.class, args);
+	}
+
+}
