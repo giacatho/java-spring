66002fcb6a22728cac966e3ef4123b1f698d926c
giacatho <giacatho@gmail.com>
2022-10-26 11:29:47 +0800

    [internalization demo] Add new Spring Internationalization demo project

---
+++ spring-internalization-demo/src/main/java/sg/nus/iss/internalization/demo/SpringInternalizationDemoApplication.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.internalization.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInternalizationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringInternalizationDemoApplication.class, args);
+	}
+
+}
---
+++ spring-internalization-demo/src/test/java/sg/nus/iss/internalization/demo/SpringInternalizationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.nus.iss.internalization.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInternalizationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
