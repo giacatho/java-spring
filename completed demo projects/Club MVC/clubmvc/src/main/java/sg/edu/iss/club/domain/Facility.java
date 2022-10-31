3b3837962ae01e1111d2738a02c911c51d759137
giacatho <giacatho@gmail.com>
2022-10-31 17:07:36 +0800

    Add club project from Suria

---
+++ completed demo projects/Club MVC/clubmvc/src/main/java/sg/edu/iss/club/domain/Facility.java
@@ -0,0 +1,56 @@
+package sg.edu.iss.club.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Facility {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String name;
+  private String description;
+
+  public Facility() {
+    super();
+    // TODO Auto-generated constructor stub
+  }
+
+  public Facility(String name, String description) {
+    super();
+    this.name = name;
+    this.description = description;
+  }
+  
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public String toString() {
+    return "Facility [id=" + id + ", name=" + name + ", description=" + description + "]";
+  }
+
+}
