a02105da36d07e9e9011fdb8e24fd0d225c75367
giacatho <giacatho@gmail.com>
2022-11-12 22:40:08 +0800

    (cts) Start creating cts by copying codes from cats

---
+++ completed demo projects/CTS/course-tracking-system/src/main/java/sg/nus/iss/cts/repository/RoleRepository.java
@@ -0,0 +1,17 @@
+package sg.nus.iss.cts.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import sg.nus.iss.cts.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, String>{
+  @Query("SELECT r.name FROM Role r")
+  List<String> findAllRolesNames();
+  
+  @Query("SELECT r FROM Role r WHERE r.name = :name")
+  List<Role> findRoleByName(@Param("name") String name);
+}
