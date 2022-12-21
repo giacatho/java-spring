f78fdba958ad68fddc7865b8f1bd5432c2f23d07
giacatho <giacatho@gmail.com>
2022-12-21 10:01:03 +0800

    (interceptor demo) ...

--- spring-interceptor-demo/src/main/java/sg/edu/nus/iss/interceptor/demo/interceptor/TimeExecuteInterceptor.java
+++ spring-interceptor-demo/src/main/java/sg/edu/nus/iss/interceptor/demo/interceptor/TimeExecuteInterceptor.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Component
 public class TimeExecuteInterceptor implements HandlerInterceptor {
+  @Override
   public boolean preHandle(HttpServletRequest request, 
             HttpServletResponse response, Object handler) {
     long startTime = System.currentTimeMillis();
@@ -17,6 +18,7 @@ public class TimeExecuteInterceptor implements HandlerInterceptor {
     return true;
   }
 
+  @Override
   public void postHandle(HttpServletRequest request, 
          HttpServletResponse response, Object handler, ModelAndView modelAndView) {
     System.out.println(request.getRequestURL());
