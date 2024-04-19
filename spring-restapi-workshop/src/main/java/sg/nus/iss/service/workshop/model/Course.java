85d852b6aea4951538272177b517c7711e368afb
giacatho <giacatho@users.noreply.github.com>
2024-04-19 18:33:47 +0800

    (restapi workshop) Convert to 3.2.5, complete the workshop as description

--- spring-restapi-workshop/src/main/java/sg/nus/iss/service/workshop/model/Course.java
+++ spring-restapi-workshop/src/main/java/sg/nus/iss/service/workshop/model/Course.java
@@ -1,9 +1,9 @@
 package sg.nus.iss.service.workshop.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Course {
--- spring-restapi-workshop/src/main/java/sg/nus/iss/service/workshop/service/CourseServiceImpl.java
+++ spring-restapi-workshop/src/main/java/sg/nus/iss/service/workshop/service/CourseServiceImpl.java
@@ -3,7 +3,7 @@ package sg.nus.iss.service.workshop.service;
 import java.util.List;
 import java.util.Optional;
 
-import javax.annotation.Resource;
+import jakarta.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
