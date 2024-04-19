24cf524af0b72601792331ba208e7a6778cb084a
giacatho <giacatho@users.noreply.github.com>
2024-04-19 10:30:21 +0800

    Convert to Spring 3.2.5
    Convert to Spring 3.2.5

--- spring-session-workshop/src/main/java/sg/nus/iss/session/workshop/TodoController.java
+++ spring-session-workshop/src/main/java/sg/nus/iss/session/workshop/TodoController.java
@@ -3,13 +3,13 @@ package sg.nus.iss.session.workshop;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class TodoController {
